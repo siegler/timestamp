c824f38e05be0aa406f46f77e8e33e439df34a6b
Chris Siegler <sieglerc@gmail.com>
2018-05-21 14:13:56 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const moment = require('moment')
+
+//A few common date formats to search for
+const formats = [
+    "MMMM D, YYYY",
+    "MM-DD-YYYY",
+    "M-DD-YYYY",
+    "M-D-YYYY"
+];
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.get('/:data', (req, res) => {
+    var data = req.params.data;
+    var output;
+    if (data.match("^[0-9]+$")) {
+        output = moment.utc(data, "X");
+    } else {
+        output = moment.utc(data, formats, true);
+    }
+
+    if (output.isValid()) {
+        res.json({
+            natural: output.format("MMMM D, YYYY"),
+            unix: output.format("X")
+        })
+    } else {
+        res.json({
+            natural: null,
+            unix: null
+        })
+    }
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000!')
+})
